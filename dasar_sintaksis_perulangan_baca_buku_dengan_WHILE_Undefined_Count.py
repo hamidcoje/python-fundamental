36d51a1468c5442acca986b081d86da8dc9c359b
hamidcoje <nurhamid993@gmail.com>
2022-01-14 15:03:12 +0700

    perulangan sampai paham dengan for dan while untuk jumlah buku tertentu, contoh: 10

---
+++ dasar_sintaksis_perulangan_baca_buku_dengan_WHILE_Undefined_Count.py
@@ -0,0 +1,17 @@
+"""
+Program membaca buku dengan while sampai paham
+"""
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu"')
+
+jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+
+while jumlah_buku_yang_sudah_dibaca_dan_dipahami < jumlah_buku:
+    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} belum paham")
+    else:
+        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
